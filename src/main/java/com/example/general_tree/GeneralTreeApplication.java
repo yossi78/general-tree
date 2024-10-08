92e32197b4665368aeae66a9be9264e61a773689
yossi78 <yossi78@gmail.com>
2024-10-08 13:49:34 +0300

    First commit

---
+++ src/main/java/com/example/general_tree/GeneralTreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.general_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/general_tree/GeneralTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.general_tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
